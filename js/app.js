eb29cad9c49664da9b5c2449e2145158275f4322
Andy <rosario.andrew@gmail.com>
2014-11-11 13:02:22 -0600

    Recoding rental price calculator

--- js/app.js
+++ js/app.js
@@ -2,44 +2,73 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-// Show/Hide Calendar
-$(document).on('click', '#checkAvail', function() {
-	$('#checkAvail').toggleClass('.hidden');
-});
-
+// Rental Price Calculator
 var rentalPrice = 0;
-var weekDay = 250;
-var weekEnd = 450;
-var weeklyPrice = 2000;
+var weekDay = 1;
+var weekEnd = 10;
+var weeklyPrice = 100;
+var cleaningFee = 125;
+var tax = 1;
+var extraGuestFee = 20;
+
+var daysPrice = function(day) {
+	console.log("Start daysPrice")
+	console.log("Function Stay Length = " + findDays(numDays))
+	for(i = firstDay; i <= firstDay + findDays(numDays) - 1; i++) {
+		if(i === 0) {
+			console.log("added sunday")
+			rentalPrice += weekDay
+		}
+		else if(i % 6 === 0 || i % 5 === 0) {
+			rentalPrice += weekEnd
+			console.log("added weekend")
+		}
+		else {
+			console.log("i = " + i)
+			rentalPrice += weekDay
+			console.log("added weekday")
+		}
+	}
+}
+
+var findDays = function(days) {
+	return days%7
+};
+
+var findWeeks = function(days) {
+	return Math.floor(days/7)
+};
+
+var firstDay;
+var numDays;
 
 function getQuote(form) {
+	console.log("Start getQuote")
 	// Remove Old Quotes
 	$('.quoteWrapper').remove();
 	// Find Rental Rate
 	rentalPrice = 0;
 	var fullDate = new Date(form.checkIn.value.substring(6,10), form.checkIn.value.substring(0,2)-1, form.checkIn.value.substring(3,5));
-	var firstDay = fullDate.getDay();
+	firstDay = fullDate.getDay();
 	var d1 = new Date (form.checkIn.value);
 	var d2 = new Date (form.checkOut.value);
-	var numDays = (d2 - d1)/(60*24*60*1000);
-	for(i = firstDay; i <= numDays+firstDay-1; i++) {
-		if(i === 0) {
-			rentalPrice = rentalPrice + weekDay
-		}
-		else if(i % 6 === 0 || i % 5 === 0) {
-			rentalPrice = rentalPrice+ weekEnd
-		}
-		else {
-			rentalPrice = rentalPrice + weekDay
-		}
-		console.log("Rental Price is " + rentalPrice);
+	numDays = (d2 - d1)/(60*24*60*1000);
+	console.log("fullDate is " + fullDate);
+	console.log("firstDay is " + firstDay);
+	if (numDays >= 7) {
+		rentalPrice += (findWeeks(numDays) * weeklyPrice)
+		daysPrice(firstDay)
 	}
-	// Find Extra Guests
+	else {
+		daysPrice(firstDay)
+	}
 	var extraGuests = form.numGuests.value;
-	// Find Full Price
-	var fullPrice = rentalPrice + 125 + ((extraGuests)*20);
+	var fullPrice = (rentalPrice + cleaningFee + (extraGuests*extraGuestFee*numDays)) * tax
+	console.log('rentalprice =  ' + rentalPrice)
+	console.log('fullPrice =  ' + fullPrice)
+};
 
-	// Makes sure all form imputs are filled out - Prints Quote
+// Makes sure all form imputs are filled out - Prints Quote
 	if(form.firstName.value === "First Name" || form.lastName.value === "Last Name" || form.email.value === "email address")
 	{
 		$('.button').before('<div class="quoteWrapper small-12 large-12 column"><h3>Please provide your full name and email address</h3></div>').slideDown()
--- js/apptest.js
+++ js/apptest.js
@@ -2,29 +2,81 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-// Show/Hide Calendar
-$(document).on('click', '#checkAvail', function() {
-	$('#checkAvail').toggleClass('.hidden');
-})
+// New Don't Repeat Yourself Rental Price Code
+
+var rentalPrice = 0;
+var weekDay = 1;
+var weekEnd = 10;
+var weeklyPrice = 100;
+var cleaningFee = 125;
+var tax = 1;
+var extraGuestFee = 20;
+
+var daysPrice = function(day) {
+	console.log("Start daysPrice")
+	console.log("Function Stay Length = " + findDays(numDays))
+	for(i = firstDay; i <= firstDay + findDays(numDays) - 1; i++) {
+		if(i === 0) {
+			console.log("added sunday")
+			rentalPrice += weekDay
+		}
+		else if(i % 6 === 0 || i % 5 === 0) {
+			rentalPrice += weekEnd
+			console.log("added weekend")
+		}
+		else {
+			console.log("i = " + i)
+			rentalPrice += weekDay
+			console.log("added weekday")
+		}
+	}
+}
+
+var findDays = function(days) {
+	return days%7
+};
+
+var findWeeks = function(days) {
+	return Math.floor(days/7)
+};
+
+var firstDay;
+var numDays;
+
+function getQuote(form) {
+	console.log("Start getQuote")
+	// Remove Old Quotes
+	$('.quoteWrapper').remove();
+	// Find Rental Rate
+	rentalPrice = 0;
+	var fullDate = new Date(form.checkIn.value.substring(6,10), form.checkIn.value.substring(0,2)-1, form.checkIn.value.substring(3,5));
+	firstDay = fullDate.getDay();
+	var d1 = new Date (form.checkIn.value);
+	var d2 = new Date (form.checkOut.value);
+	numDays = (d2 - d1)/(60*24*60*1000);
+	console.log("fullDate is " + fullDate);
+	console.log("firstDay is " + firstDay);
+	if (numDays >= 7) {
+		rentalPrice += (findWeeks(numDays) * weeklyPrice)
+		daysPrice(firstDay)
+	}
+	else {
+		daysPrice(firstDay)
+	}
+	var extraGuests = form.numGuests.value;
+	var fullPrice = (rentalPrice + cleaningFee + (extraGuests*extraGuestFee*numDays)) * tax
+	console.log('rentalprice =  ' + rentalPrice)
+	console.log('fullPrice =  ' + fullPrice)
+	}
+
+
+// Old Rental Quote Price
+
 
 var rentalPrice = 0;
 var weekDay = 250;
 var weekEnd = 450;
 var weeklyPrice = 2000;
-// var extraGuests = this.form.numGuests.value
-
-// function firstDay (form) {
-// var fullDate = new Date(form.checkIn.value);
-// console.log(fullDate);
-// var d = fullDate.getDay();
-// return(d);
-// }
-
-// function numDays(form) {
-// 	var d1 = new Date (form.checkIn.value);
-// 	var d2 = new Date (form.checkOut.value);
-// 	return((d2 - d1)/(60*24*60*1000));
-// }
 
 function getQuote(form) {
 	// Remove Old Quotes
@@ -32,75 +84,23 @@ function getQuote(form) {
 	// Find Rental Rate
 	rentalPrice = 0;
 	var fullDate = new Date(form.checkIn.value.substring(6,10), form.checkIn.value.substring(0,2)-1, form.checkIn.value.substring(3,5));
-	// console.log(form.checkIn.value.substring(3,5));
-	// console.log(form.checkIn.value.substring(5,7));
-	// console.log(form.checkIn.value.substring(8,10));
-	// console.log(form.checkIn.value);
-	// console.log(fullDate);
 	var firstDay = fullDate.getDay();
-	// console.log("Day of the week is " + firstDay);
 	var d1 = new Date (form.checkIn.value);
 	var d2 = new Date (form.checkOut.value);
 	var numDays = (d2 - d1)/(60*24*60*1000);
-	// console.log("The stay is " + numDays + " nights");
-	// console.log("stayLength is" + stayLength);
-	// console.log(firstDay);
-	// console.log(typeof(firstDay));
-	if(numDays >= 7) {
 	for(i = firstDay; i <= numDays+firstDay-1; i++) {
 		if(i === 0) {
 			rentalPrice = rentalPrice + weekDay
 		}
 		else if(i % 6 === 0 || i % 5 === 0) {
 			rentalPrice = rentalPrice+ weekEnd
-			// console.log(rentalPrice)
 		}
 		else {
 			rentalPrice = rentalPrice + weekDay
-			// console.log(rentalPrice)
 		}
 		console.log("Rental Price is " + rentalPrice);
 	}
 	// Find Extra Guests
 	var extraGuests = form.numGuests.value;
-	// console.log(extraGuests);
-	// console.log(form.checkIn.value);
-	// console.log(form.checkOut.value);
+	// Find Full Price
 	var fullPrice = rentalPrice + 125 + ((extraGuests)*20);
-	// console.log("The full price is $" + fullPrice.toFixed(2));
-	
-
-		$('.button').before('<div class="quoteWrapper small-12 large-12 column"><h3>Your request has been sent. <br> The estimated price is $' + fullPrice.toFixed(2) + '</h3></div>')
-		$('#quoteForm').submit()
-
-};
-
-$(document).on('focus', '.blur', function(){
-	console.log("click");
-	$(this).removeAttr('value');
-});
-
-$(document).on('blur', '.blur', function(){
-	// console.log("blug");
-	// console.log(this.name);
-	if(this.value === "") {
-		if(this.name === "firstName") {
-			$(this).attr('value', 'First Name')
-		}
-		else if(this.name === 'lastName') {
-			$(this).attr('value', 'Last Name')
-		}
-		else if(this.name === 'email') {
-			$(this).attr('value', 'email address')
-		}
-		else if(this.name === 'checkIn' || this.name === 'checkOut') {
-			$(this).attr('value', 'mm/dd/yyyy')
-		}
-		else {
-			$(this).attr('value', 'Get a Quote')
-		};
-
-	};
-
-});
-
